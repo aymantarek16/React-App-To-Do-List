cdf5a965a9007dae689fb6a6d205c621c3feab81
aymantarek16 <aymantarek16@yahoo.com>
2022-06-11 17:17:26 +0200

    Build Header Of App

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "./components/Header";
+import "./App.scss";
+const App = () => {
+  return (
+    <div className="container">
+      <div className="app-wrapper">
+        <div>
+          <Header />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
