8215e036131dcd7215414374f5cb745e66773caf
aymantarek16 <aymantarek16@yahoo.com>
2022-06-11 18:05:57 +0200

    Add Form Component

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,20 +1,16 @@
 import React from "react";
 import { v4 as uuidv4 } from "uuid";
 
-
-
 const Form = ({ input, setInput, todos, setTodos }) => {
-
   const onInputChange = (event) => {
     setInput(event.target.value);
   };
-  
+
   const onFormSubmit = (e) => {
     e.preventDefault();
     setTodos([...todos, { id: uuidv4(), title: input, completed: false }]);
     setInput("");
   };
-  
 
   return (
     <form onSubmit={onFormSubmit}>
