71b6ab971708cb84d1d6512225ac5ea666870e8c
aymantarek16 <aymantarek16@yahoo.com>
2022-06-11 15:24:16 +0200

    minify

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.scss";
-import TodoList from './components/TodoList';
+import TodoList from './TodoList';
 
 ReactDOM.render(
   <React.StrictMode>
