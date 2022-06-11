fdbe53944910080aaf61566468f264346965d597
aymantarek16 <aymantarek16@yahoo.com>
2022-06-11 14:54:17 +0200

    switch to onther projects ..

---
+++ src/TodoList.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import  'TodoList.scss'
+
+const TodoList = () => {
+  return (
+    <div>TodoList</div>
+  )
+}
+
+export default TodoList
--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.scss";
+import TodoList from './components/TodoList';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <TodoList />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
